83f3b0760add199589c946c0233fd5c37878b0d1
minibassito <minibassito@hotmail.com>
2022-06-23 07:49:45 -0500

    first commit

---
+++ public/pantalla.js
@@ -0,0 +1,41 @@
+const socket = io()
+
+let pantalla = document.getElementById("pantalla");
+let action = document.getElementById("action");
+let horario = document.getElementById("horario");
+
+function recarga() {
+    document.getElementById("pantalla").innerHTML = "";
+    document.getElementById("action").innerHTML = "";
+    document.getElementById("horario").innerHTML = "";
+}
+
+function jarvis(data) {
+    var speech = new SpeechSynthesisUtterance;
+    
+    speech.text = data.paciente + 'consultorio' + data.consultorio;
+    speech.volume = 6;
+    speech.rate = 0.7;
+    speech.pitch = 1.5;
+    window.speechSynthesis.speak(speech);
+}
+    
+
+socket.on("datos:captura", function (data) {
+    recarga();
+    jarvis(data);
+    jarvis(data);
+    jarvis(data);
+    jarvis(data);
+
+    pantalla.innerHTML += `<p>
+        <strong>${data.paciente}</strong>
+    </p>`
+    action.innerHTML += `<p>
+        ${data.consultorio}
+    </p>`
+    horario.innerHTML += `<p>
+        ${data.horario}
+    </p>`
+    
+});
